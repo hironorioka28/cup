83cef539541d3ca08e8c637eb0baa62c8400ba02
Hironori Oka <hironorioka28@gmail.com>
2015-06-27 16:27:19 +0900

    PHP

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@
 <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+<? include 'header.php'; ?>
 <h1>Hello Tumblr</h1>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 <script src="assets/js/script.js"></script>
