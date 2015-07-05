fab6c929e159840a52a34378879e450806e07d22
Hironori Oka <hironorioka28@gmail.com>
2015-07-05 20:14:01 +0900

    about, contact

--- web/header.php
+++ web/header.php
@@ -1,3 +1,21 @@
-<header>
-<p>これはヘッダーですお</p>
+<div id="header-wrap" class="menutype_horizontal_header">
+<header class="clearfix">
+<h1 class="blogtitle">
+<a href="/" title="CUPWORKS"><img src="http://static.tumblr.com/a8939a85746eedfe14281f56954bdb24/2ye9z2c/AyZnpm9ab/tumblr_static_1cr0p3xf5npcw40kwog0og8g4.png" alt="cupworks" width="160"></a>
+</h1><!-- /.blogtitle -->
+<div class="menutype_horizontal">
+<div class="menu-toggle">
+<span class="icon-menu"></span>
+</div><!-- /.menu-toggle -->
+<nav class="the-menu">
+<ul class="main-menu">
+<li><a href="/projects.php" title="Projects">Projects</a></li>
+<li><a href="/news.php" title="News">News</a></li>
+<li><a href="/profiel.php" title="Profiel">Profiel</a></li>
+<li><a href="/contact.php" title="Contact">Contact</a></li>
+<li><a href="/about.php" title="About">About</a></li>
+</ul>
+</nav>
+</div><!-- /.menutype_horizontal -->
 </header>
+</div><!-- /#header-wrap -->
--- web/index.php
+++ web/index.php
@@ -1,11 +1,25 @@
 <!doctype html>
 <html lang="ja">
 <head>
+<link rel="stylesheet" href="http://static.tumblr.com/wdg4six/SLGn2kwl9/bakedpixel-icons.css">
+<link rel="stylesheet" href="http://static.tumblr.com/wdg4six/Y0wn2gegv/rightbox.css">
 <link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="http://assets.tumblr.com/fonts/garamondclassicfs/stylesheet.css?v=3">
 </head>
 <body>
 <? include 'header.php'; ?>
-<h1>Hello Tumblr</h1>
+<section>
+<h1>Cupworks</h1>
+<nav>
+<ul>
+<li><a href="top.php">Top</a></li>
+<li><a href="projects.php">Projects</a></li>
+<li><a href="news.php">News</a></li>
+<li><a href="about.php">About</a></li>
+<li><a href="contact.php">Contact</a></li>
+</ul>
+</nav>
+</section>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 <script src="assets/js/script.js"></script>
 </body>
