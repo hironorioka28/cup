1451445ede333115fb13bf3dc5c6620f48888b92
Hironori Oka <hironorioka28@gmail.com>
2015-07-20 18:12:23 +0900

    carousel

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,3 +1,67 @@
 (function($) {
-    $("#carousel .current").css("display", "block");
+    var counter = 0,
+        timer;
+
+    startTimer();
+
+    $("#carousel .next").on("click", function() {
+        stopTimer();
+        slideShow(false);
+    });
+    $("#carousel .prev").on("click", function() {
+        stopTimer();
+        slideShow(true);
+    });
+
+    $("#testButton .start").on("click", function() {
+        console.log("start");
+        startTimer();
+    });
+
+    $("#testButton .stop").on("click", function() {
+        console.log("stop");
+        stopTimer();
+    });
+
+    $("#thumbnail button").on("click", function() {
+        stopTimer();
+        counter = $(this).parent().index();
+        $("#carousel .mainImg li").removeClass("current").eq(counter).addClass("current");
+        $("#carousel .thumbnail li").removeClass("current").eq(counter).addClass("current");
+        $("#carousel .mainImg li").eq(counter).addClass("current");
+        $("#carousel .thumbnail li").eq(counter).addClass("current");
+    });
+
+    function startTimer() {
+        timer = setInterval(function() {
+            slideShow(false);
+        }, 3000);
+    }
+
+    function stopTimer() {
+        clearInterval(timer);
+    }
+
+    function slideShow(reverse) {
+        if (reverse) {
+            counter--;
+        } else {
+            counter++;
+        }
+        $("#carousel .mainImg li").removeClass("current").eq(counter).addClass("current");
+        $("#carousel .thumbnail li").removeClass("current").eq(counter).addClass("current");
+        if (reverse) {
+            if (counter < 0) {
+                counter = $("#carousel .mainImg li").length - 1;
+                counter = $("#carousel .thumbnail li").length - 1;
+            }
+        } else {
+            if (counter > $("#carousel .mainImg li").length - 1) {
+                counter = 0;
+            }
+        }
+        $("#carousel .mainImg li").eq(counter).addClass("current");
+        $("#carousel .thumbnail li").eq(counter).addClass("current");
+    }
+
 })(jQuery);
--- web/index.php
+++ web/index.php
@@ -17,21 +17,56 @@
 
 <section>
   <h1>Carousel</h1>
+  <div id="testButton">
+    <button type="button" class="start">開始</button>
+    <button type="button" class="stop">停止</button>
+  <div><!-- /#testButton  -->
   <div id="carousel" class="carouselWrapper">
     <div class="prevNextWrapper">
       <button type="button" class="prev">prev</button>
     </div><!-- /.prevNextWrapper -->
-    <ul>
-      <li class="current" style="background-image:url(assets/img/kawa.jpg)"></li>
-      <li style="background-image:url(assets/img/kinoko.jpg)"></li>
-      <li style="background-image:url(assets/img/sawa.jpg)"></li>
-      <li style="background-image:url(assets/img/taki.jpg)"></li>
-      <li style="background-image:url(assets/img/inu.jpg)"></li>
-    </ul>
     <div class="prevNextWrapper">
       <button type="button" class="next">next</button>
     </div><!-- /.prevNextWrapper -->
-  </div><!-- /.carouselWrapper -->
+    <div class="mainImg">
+      <ul>
+        <li class="current">
+          <div class="imgWrapper" style="background-image:url(assets/img/kawa.jpg)"></div>
+          <h2>11111111111111111111</h2>
+          <p>説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文</p>
+        </li>
+        <li>
+          <div class="imgWrapper" style="background-image:url(assets/img/kinoko.jpg)"></div>
+          <h2>2222222222222222222</h2>
+          <p>説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文</p>
+        </li>
+        <li>
+          <div class="imgWrapper" style="background-image:url(assets/img/sawa.jpg)"></div>
+          <h2>3333333333333333333</h2>
+          <p>説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文</p>
+        </li>
+        <li>
+          <div class="imgWrapper" style="background-image:url(assets/img/taki.jpg)"></div>
+          <h2>444444444444444444</h2>
+          <p>説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文</p>
+        </li>
+        <li>
+          <div class="imgWrapper" style="background-image:url(assets/img/inu.jpg)"></div>
+          <h2>5555555555555555555</h2>
+          <p>説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文</p>
+        </li>
+      </ul>
+    </div><!-- /.mainImg -->
+    <div id="thumbnail" class="thumbnail">
+      <ul>
+        <li class="current" style="background-image:url(assets/img/kawa.jpg)"><button type="button"></button></li>
+        <li style="background-image:url(assets/img/kinoko.jpg)"><button type="button"></button></li>
+        <li style="background-image:url(assets/img/sawa.jpg)"><button type="button"></button></li>
+        <li style="background-image:url(assets/img/taki.jpg)"><button type="button"></button></li>
+        <li style="background-image:url(assets/img/inu.jpg)"><button type="button"></button></li>
+      </ul>
+    </div><!-- /#thumbnail -->
+  </div><!-- /#carousel -->
 </section>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 <script src="assets/js/script.js"></script>
